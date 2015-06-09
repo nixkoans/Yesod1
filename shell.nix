42ed343de881552d2522b8ba1e39168bfc28a85f
Calvin Cheng <linchuan.cheng@gmail.com>
2015-06-09 15:50:23 +0800

    Magically generated shell.nix and default.nix from cabal2nix

---
+++ shell.nix
@@ -0,0 +1,44 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, base, bytestring, classy-prelude
+      , classy-prelude-conduit, classy-prelude-yesod, conduit, containers
+      , data-default, directory, fast-logger, file-embed, hjsmin, hspec
+      , http-conduit, monad-control, monad-logger, persistent
+      , persistent-sqlite, persistent-template, resourcet, safe
+      , shakespeare, stdenv, template-haskell, text, time, transformers
+      , unordered-containers, vector, wai-extra, wai-logger, warp, yaml
+      , yesod, yesod-auth, yesod-core, yesod-form, yesod-static
+      , yesod-test
+      }:
+      mkDerivation {
+        pname = "Yesod1";
+        version = "0.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        buildDepends = [
+          aeson base bytestring classy-prelude classy-prelude-conduit
+          classy-prelude-yesod conduit containers data-default directory
+          fast-logger file-embed hjsmin http-conduit monad-control
+          monad-logger persistent persistent-sqlite persistent-template safe
+          shakespeare template-haskell text time unordered-containers vector
+          wai-extra wai-logger warp yaml yesod yesod-auth yesod-core
+          yesod-form yesod-static
+        ];
+        testDepends = [
+          base classy-prelude classy-prelude-yesod hspec monad-logger
+          persistent persistent-sqlite resourcet shakespeare transformers
+          yesod yesod-core yesod-test
+        ];
+        license = stdenv.lib.licenses.unfree;
+      };
+
+  drv = pkgs.haskell.packages.${compiler}.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
